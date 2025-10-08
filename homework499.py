a1d55b17c06fb47d656553942d0117f9c2471700
helder04 <neutronex966@protonmail.com>
2025-10-08 16:51:16 +0400

    Add files via upload

---
+++ homework499.py
@@ -0,0 +1,53 @@
+mom = "Leila Tevzadze"
+dad = "Merab Kopaleishvili"
+cat = "Gibraltar"
+game = "Counter-Strike Source"
+music_artist = "Kanye West"
+print("Hey, My name's Giorgi. I don't really know how to write biographies so I'll just wing it. My mom's name is", mom, "and my dad's name is", dad,
+      ".", "I don't have any siblings. Any other supposed family I have is my cat", cat, "I don't really play alot of games, but my favorite game is probably", game, ". Although, I don't play it nearly as much as I used to. On the other hand," \
+      "something I do alot is listen to music. My favorite artist is probably", music_artist, "I've been listening to him for a while, and I enjoy it alot.") 
+# :p
+print(5 + 5)
+print(2 + 5)
+print (4 + 12)
+print (6 + 7)
+print (4 + 1)
+print (4 * 4)
+print (8 * 9)
+print (5 * 2)
+print (9 * 4)
+print (8 * 64)
+print (7 / 2)
+print (7 / 1)
+print (6 / 7)
+print (40 / 21)
+print (23 / 12)
+print (12 - 3)
+print (14 - 9)
+print (75 - 43)
+print (23 - 12)
+print (32 - 19)
+print (12 // 5)
+print (91 // 23)
+print (23 // 9.1)
+print (663 // 12)
+print (820 // 13)
+# :p
+balance = 100
+continent = "europe"
+reward = 50
+
+print(type(balance))
+print(type(continent))
+# print(type(height))
+#The debugger isn't necessary here, there's no code that gives the variable name "height" an actual assigned value. It's just dead weight.
+#Instead, "height" should be replaced with reward. 
+#So the correct code is
+print(type(reward))
+#That's all
+#Now we continue as we would usually :p
+print("Total:", balance + reward)
+print("Subtraction:", balance - reward)
+print("Multiply:", balance * reward)
+print("Divide:", balance / reward)
+#Nice
